2bacde69ed1958ff37e86e0724f2d41c1e9883d7
MANAN TANDON <manantandon@gmail.com>
2024-10-06 17:09:59 +0530

    feed card ui resolved + module css for NgoCard

--- src/components/Cards/NgoCard.jsx
+++ src/components/Cards/NgoCard.jsx
@@ -1,3 +1,4 @@
+import { text } from "@/constant/testData";
 import { Add } from "@mui/icons-material";
 import {
   Box,
@@ -7,105 +8,45 @@ import {
   CardMedia,
   Typography,
 } from "@mui/material";
-import Image from "next/image";
 import React, { useState } from "react";
+
+import NgoCardStyles from "@/styles/NgoCards.module.css";
 
 export const NgoCard = ({ color }) => {
   const [show, setShow] = useState(false);
-  const text = `Prosperity for Humanity, as defined for an NGO, embodies a holistic vision of well-being that integrates economic, social, and
-            environmental dimensions. It seeks to create conditions where
-            individuals and communities thrive through access to fair wages,
-            sustainable livelihoods, and educational opportunities. Key aspects
-            include promoting social equity, protecting natural resources, and
-            empowering communities to make informed decisions. Additionally, it
-            values cultural diversity and fosters intercultural understanding.
-            By addressing these interconnected areas, an NGO can work towards a
-            more equitable and sustainable future, ensuring that progress
-            benefits everyone and enhances overall quality of life.`;
 
   const handleShow = () => {
     setShow(!show);
   };
   return (
     <>
-      <Card
-        sx={{
-          my: 1,
-          borderRadius: "20px",
-          position: "relative",
-        }}
-        variant="outlined"
-      >
+      <Card className={NgoCardStyles.ngoc1} variant="outlined">
         <CardMedia
-          sx={{ height: 200 }}
+          className={NgoCardStyles.ngoc2}
           image="/images/ngo.png"
           title="NGO IMAGE"
         />
-        <CardContent
-          sx={{
-            padding: 0,
-            "&: last-child": {
-              padding: 0,
-            },
-          }}
-        >
-          <Box sx={{ my: 1, px: 2 }}>
+        <CardContent className={NgoCardStyles.ngoc3}>
+          <Box sx={{ my: 1 }} px={2}>
             <Typography fontSize="12px" fontWeight="700" sx={{ color: color }}>
               Community Development
             </Typography>
           </Box>
-          <Box sx={{ px: 2 }}>
+          <Box px={2}>
             <Typography fontSize="25px" fontWeight="bold" sx={{ my: 1 }}>
               Prosperity For Humanity
             </Typography>
-            <Typography
-              sx={{
-                overflow: "hidden",
-                textOverflow: "ellipsis",
-                display: "-webkit-box",
-                WebkitLineClamp: "3",
-                WebkitBoxOrient: "vertical",
-              }}
-              fontSize="14px"
-            >
+            <Typography className={NgoCardStyles.ngoc4} fontSize="14px">
               {text}
             </Typography>
           </Box>
-          <Box
-            sx={{
-              px: 2,
-              py: 1.5,
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-              mt: 2.2,
-            }}
-          >
-            <Button
-              sx={{
-                bgcolor: "rgb(19,32,48)",
-                color: "#fff",
-                borderRadius: "10px",
-                px: 4,
-              }}
-            >
-              Know More
-            </Button>
+          <Box className={NgoCardStyles.ngoc5} px={2} py={1.5}>
+            <Button className={NgoCardStyles.ngoc6}>Know More</Button>
             <Box
-              sx={{
-                bgcolor: "rgb(19,32,48)",
-                color: "#fff",
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                borderRadius: "50%",
-                p: 0.2,
-                zIndex: 100,
-                "&:hover": {
-                  cursor: "pointer",
-                },
-              }}
-              className={show ? "crossCircleRotate" : "crossCircle"}
+              p={0.2}
+              className={`${NgoCardStyles.ngoc7} ${
+                show ? NgoCardStyles.ngoc8 : NgoCardStyles.ngoc9
+              }`}
               onClick={handleShow}
             >
               <Add />
@@ -113,15 +54,11 @@ export const NgoCard = ({ color }) => {
           </Box>
           <Box
             sx={{
-              position: "absolute",
-              height: "100%",
-              width: "100%",
               bgcolor: color,
-              borderRadius: "20px",
-              top: 0,
-              left: 0,
             }}
-            className={`fade-box ${show ? "fade-in" : "fade-out"}`}
+            className={`${NgoCardStyles.ngoc10} fade-box ${
+              show ? "fade-in" : "fade-out"
+            }`}
           ></Box>
         </CardContent>
       </Card>
---
+++ src/constant/testData.jsx
@@ -0,0 +1,10 @@
+export const text = `Prosperity for Humanity, as defined for an NGO, embodies a holistic vision of well-being that integrates economic, social, and
+            environmental dimensions. It seeks to create conditions where
+            individuals and communities thrive through access to fair wages,
+            sustainable livelihoods, and educational opportunities. Key aspects
+            include promoting social equity, protecting natural resources, and
+            empowering communities to make informed decisions. Additionally, it
+            values cultural diversity and fosters intercultural understanding.
+            By addressing these interconnected areas, an NGO can work towards a
+            more equitable and sustainable future, ensuring that progress
+            benefits everyone and enhances overall quality of life.`;
